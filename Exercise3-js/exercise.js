d50ff22299894be4365a4adefaa25dd657cc8349
Tram <tram.pham@asiantech.vn>
2017-12-14 23:20:00 +0700

    fix convention

--- Exercise3-js/exercise.js
+++ Exercise3-js/exercise.js
@@ -1,53 +1,53 @@
 //1. addEventListener()
-var idEl=document.getElementById("btn-1");
-idEl.addEventListener ("click",addEventLs);
+var idEl=document.getElementById('btn-1');
+idEl.addEventListener ('click',addEventLs);
 function addEventLs() {
-  alert ("Example about addEventListener");
+  alert ('Example about addEventListener');
 }
 //2. removeEventListener()
-var idEl2=document.getElementById("btn-2");
-idEl2.addEventListener("click",rmListener);
-idEl2.removeEventListener("dbclick",rmListener);
+var idEl2=document.getElementById('btn-2');
+idEl2.addEventListener('click',rmListener);
+idEl2.removeEventListener('dbclick',rmListener);
 function rmListener() {
-  alert ("Example about removeEventListener");
+  alert ('Example about removeEventListener');
 }
 //3&4. createElement() and appendChild()
 function createEl() {
-  var btn=document.createElement("BUTTON");
-  document.getElementById("btn-3").appendChild(btn);
+  var btn=document.createElement('BUTTON');
+  document.getElementById('btn-3').appendChild(btn);
 }
 //5. removeChild()
 function rmChild() {
-  var list=document.getElementById("btn-5");
+  var list=document.getElementById('btn-5');
   list.removeChild(list.childNodes[2]);
 }
 //6. replaceChild()
 function rplChild() {
-  var newelm = document.createElement("li");
-  var textnode = document.createTextNode("Water");
+  var newelm = document.createElement('li');
+  var textnode = document.createTextNode('Water');
   newelm.appendChild(textnode);
-  var item = document.getElementById("btn-5");
+  var item = document.getElementById('btn-5');
   item.replaceChild(newelm, item.childNodes[1]);
 }
 //7. cloneNode()
 function clnNode() {
-  var itm = document.getElementById ("btn-6");
+  var itm = document.getElementById ('btn-6');
   var cln = itm.cloneNode (true);
-  document.getElementById("btn-5").appendChild(cln);
+  document.getElementById('btn-5').appendChild(cln);
 }
 //9. setAttribute()
 function setAttr() {
-  var tagEl=document.getElementsByTagName("h2")[0];
-  tagEl.setAttribute("class","democlass");
+  var tagEl=document.getElementsByTagName('h2')[0];
+  tagEl.setAttribute('class','democlass');
 }
 //10. getAttribute()
 function getAttr() {
-  var x=document.getElementById("btn-7").getAttribute("href");
-  document.getElementById("result").innerHTML = x;
+  var x=document.getElementById('btn-7').getAttribute('href');
+  document.getElementById('result').innerHTML = x;
 }
 
 //11. removeAttribute()
 function rmAttr() {
-  document.getElementsByTagName("h2")[0].removeAttribute("class");
+  document.getElementsByTagName('h2')[0].removeAttribute('class');
 } 
  
